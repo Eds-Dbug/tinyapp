d36872a6b39e1f4089893e223bdbb2eacc800c98
Eds-Dbug <edsdbug@gmail.com>
2022-08-10 00:14:15 +0000

    Finished login and logout functionality.

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const {generateRandomString} = require('./generate_string/generateRandomString')
 const app = express();
 const PORT = 8080;
@@ -13,15 +14,25 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+/********************************************************************************
+ * App Uses
+ ******************************************************************************/
+app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 
 
+/********************************************************************************
+ * GET ROUTES
+ ******************************************************************************/
 app.get('/',(req, res) => {
   res.send('Hello World!');
 });
 
 app.get('/urls',(req, res) => {
-  const templateVars = {urls: urlDatabase};
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render('urls_index', templateVars);
 });
 
@@ -35,14 +46,18 @@ app.get("/u/:id", (req, res) => {
 });
 
 app.get('/urls/new',(req, res) => {
-  res.render('urls_new');
+  const templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render('urls_new',templateVars);
 });
 
 app.get('/urls/:id',(req, res) => {
   //console.log(req.params)
 const templateVars = {
   id: req.params.id, 
-  longURL: urlDatabase[req.params.id]
+  longURL: urlDatabase[req.params.id],
+  username: req.cookies["username"]
 };
   res.render('urls_show', templateVars);
 });
@@ -54,6 +69,8 @@ app.get('/urls.json', (req, res) => {
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
+
+
 
 /********************************************************************************
  * POST ROUTES
@@ -84,6 +101,34 @@ app.post('/urls/:id/delete', (req,res)=> {
   res.redirect('/urls')
 }) 
 
+/********************************************************************************
+ * LOGIN ROUTE
+ ******************************************************************************/
+app.post('/login', (req,res) => {
+ 
+  console.log('body', req.body)
+  console.log(req.cookies);
+  res.cookie('username',req.body.username)
+  
+  res.redirect('/urls')
+})
+
+/********************************************************************************
+ * LOGOUT ROUTE
+ ******************************************************************************/
+ app.post('/logout', (req,res) => {
+ 
+  console.log('body', req.body)
+  console.log(req.cookies);
+  res.clearCookie('username',req.body.username)
+  
+  res.redirect('/urls')
+})
+
+
+/********************************************************************************
+ * LISTENER
+ ******************************************************************************/
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
